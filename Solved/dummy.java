9c31d7e86a103c754bc6072814a5a2c3621f1506
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-23 23:17:40 +0530

    Reverse a linked list

--- Solved/dummy.java
+++ Solved/dummy.java
@@ -1,40 +1,13 @@
 package Solved;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class dummy {
     public static void main(String[] args) {
-        lengthOfLongestSubstring("abcabcbb");
+        String s = "12312 sdfds asdasd";
 
-    }
+        String[] a = s.split("(?>=.)");
 
-    public static int lengthOfLongestSubstring(String s) {
-        int len = 0;
-        for(int i=0; i<s.length() - 1; i++) {
-            for(int j=i+1; j<s.length(); j++) {
-                String subStr = s.substring(i, j+1);
-
-                if(!checkRepeated(subStr)) {
-                    if(subStr.length() > len) {
-                        len = subStr.length();
-                        System.out.println(subStr);
-                    }
-                }
-            }
-        }
-
-        return len;
-    }
-
-    public static boolean checkRepeated(String v) {
-        List<String> res = new ArrayList<>();
-        for(int i=0; i<v.length(); i++) {
-            if(!res.contains(""+v.charAt(i)))
-                res.add(""+ v.charAt(i));
-            else
-                return true;
-        }
-        return false;
+        System.out.println(Arrays.toString(a));
     }
 }
