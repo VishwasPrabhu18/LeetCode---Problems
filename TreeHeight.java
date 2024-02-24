00b19814106f292e61b835b29c0a5a8ff3984c89
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-24 18:59:55 +0530

    printing node in level wise

--- TreeHeight.java
+++ TreeHeight.java
@@ -11,12 +11,32 @@ class TreeNode {
 public class TreeHeight {
     public static int getTreeHeight(TreeNode root) {
         if (root == null)
-            return -1;
+            return 0;
 
         int leftNodeHeight = getTreeHeight(root.leftNode);
         int rightNodeHeight = getTreeHeight(root.rightNode);
 
         return Math.max(leftNodeHeight, rightNodeHeight) + 1;
+    }
+
+    public static void printTreeNode(TreeNode root) {
+        int h = getTreeHeight(root);
+        for(int i=1; i<=h; i++)
+            printTreeLevelNode(root, i);
+    }
+
+    public static void printTreeLevelNode(TreeNode rooNode, int level) {
+        if(rooNode == null) 
+            return;
+        
+        if(level == 1)
+            System.out.print(rooNode.data + " ");
+        
+        else {
+            printTreeLevelNode(rooNode.leftNode, level - 1);
+            printTreeLevelNode(rooNode.rightNode, level - 1);
+        }
+
     }
     
     public static void main(String[] args) {
@@ -29,6 +49,10 @@ public class TreeHeight {
         tNode.rightNode.leftNode = new TreeNode(6);
         tNode.rightNode.leftNode.leftNode = new TreeNode(7);
 
-        System.out.println(getTreeHeight(tNode));
+        System.out.println(getTreeHeight(tNode) - 1);
+
+        System.out.println();
+
+        printTreeNode(tNode);
     }
 }
